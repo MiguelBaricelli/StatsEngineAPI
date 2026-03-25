f5f09967455fbdce8349e2afa268e9ec7add5392
Miguel Augusto Baricelli <baricellimiguel@gmail.com>
2026-03-24 23:44:10 -0300

    add project

---
+++ StatsEngineAPI.Domain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StatsEngineAPI.Domain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
